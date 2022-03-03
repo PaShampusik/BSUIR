5e5e75b82d95d3aa972bc4bc95940acfe19ee335
Pashutkaa <shchirovpavel@gmail.com>
2022-03-03 14:24:57 +0300

    add otchet

---
+++ C#/Task_1/Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Write("Enter the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = a / b;
+Console.Write("Result = {0}", c);
+
+
