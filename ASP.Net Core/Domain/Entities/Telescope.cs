f1f84c8e8bb3e1c092a53463e25719ecfe47ee6c
Shchirov Pavel <shchirovpavel@gmail.com>
2023-09-18 16:05:55 +0300

    Created Domain Project, added Entities

---
+++ ASP.Net Core/Domain/Entities/Telescope.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Domain.Entities
+{
+    public class Telescope
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category? Category{ get; set; }
+        public int FocusLength {  get; set; }       
+        public string? img {  get; set; }
+        public string mime { get; set; } 
+    }
+}
---
+++ ASP.Net Core/Domain/Models/ProductListModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class ProductListModel<T>
+    {
+        // запрошенный список объектов
+        public List<T> Items { get; set; } = new();
+        // номер текущей страницы
+        public int CurrentPage { get; set; } = 1;
+        // общее количество страниц
+        public int TotalPages { get; set; } = 1;
+    }
+}
