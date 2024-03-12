635fea88abc7f38e4dd0c8ffa04305c18fd938e2
Shchirov Pavel <shchirovpavel@gmail.com>
2024-03-12 16:49:18 +0300

    Added ddl script for tables_identical function

--- 6 sem/МДиСУБД/Lab3/Lab3.sql
+++ 6 sem/МДиСУБД/Lab3/Lab3.sql
@@ -65,3 +65,61 @@ EXCEPT dev_columns_set IS NOT EMPTY THEN v_identical := false;
 END IF;
 IF v_identical THEN RETURN v_identical;
 END IF;
+ddl_script := '';
+DECLARE data_type VARCHAR2(100);
+data_length NUMBER;
+data_precision NUMBER;
+BEGIN FOR i IN 1..dev_columns_set.count LOOP IF dev_columns_set(i) NOT MEMBER OF prod_columns_set THEN
+SELECT data_type,
+    data_length,
+    data_precision INTO data_type,
+    data_length,
+    data_precision
+FROM all_tab_columns
+WHERE owner = dev_schema_name
+    AND table_name = p_table_name
+    AND column_name = dev_columns_set(i);
+ddl_script := ddl_script || 'ALTER TABLE ' || p_table_name || ' ADD COLUMN ' || dev_columns_set(i) || ' ' || data_type;
+IF data_type IN ('VARCHAR2', 'NVARCHAR2', 'RAW')
+AND data_length IS NOT NULL THEN ddl_script := ddl_script || '(' || data_length || ')';
+ELSIF data_type IN ('NUMBER')
+AND data_precision IS NOT NULL THEN ddl_script := ddl_script || '(' || data_precision || ')';
+END IF;
+ddl_script := ddl_script || chr(10);
+END IF;
+END LOOP;
+END;
+FOR i IN 1..dev_constraints_set.count LOOP IF dev_constraints_set(i) NOT MEMBER OF prod_constraints_set THEN
+DECLARE ddl_script CLOB;
+constraint_type VARCHAR2(20);
+BEGIN
+SELECT constraint_type INTO constraint_type
+FROM all_constraints
+WHERE owner = dev_schema_name
+    AND table_name = p_table_name
+    AND constraint_name = dev_constraints_set(i);
+ddl_script := ddl_script || dbms_metadata.get_ddl(
+    CASE
+        WHEN constraint_type = 'R' THEN 'REF_CONSTRAINT'
+        ELSE 'CONSTRAINT'
+    END,
+    dev_constraints_set(i),
+    dev_schema_name
+);
+ddl_script := ddl_script || chr(10);
+END;
+END IF;
+END LOOP;
+FOR i IN 1..prod_columns_set.count LOOP IF prod_columns_set(i) NOT MEMBER OF dev_columns_set THEN ddl_script := ddl_script || 'ALTER TABLE ' || prod_schema_name || '.' || p_table_name || ' DROP COLUMN ' || prod_columns_set(i) || ';' || chr(10);
+END IF;
+END LOOP;
+FOR i IN 1..prod_constraints_set.count LOOP IF prod_columns_set(i) NOT MEMBER OF dev_constraints_set THEN ddl_script := ddl_script || 'ALTER TABLE ' || prod_schema_name || '.' || p_table_name || ' DROP CONSTRAINT ' || prod_constraints_set(i) || ';' || chr(10);
+END IF;
+END LOOP;
+dbms_output.put_line(
+    'DDL-script for updating table ' || p_table_name || ' {' || chr(10)
+);
+ddl_script := replace(ddl_script, dev_schema_name, prod_schema_name);
+dbms_output.put_line(ddl_script);
+RETURN v_identical;
+END are_tables_identical;
