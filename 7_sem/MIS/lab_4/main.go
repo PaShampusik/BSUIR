71358b95272f2837e0454c12614a3198b75218d3
PaShampusik <shchirovpavel@gmail.com>
2024-11-04 20:07:20 +0300

    lab 4 MIS done

--- 7_sem/MIS/lab_4/main.go
+++ 7_sem/MIS/lab_4/main.go
@@ -1,370 +1,386 @@
 package main
 
 import (
-	"fmt"
+	"math"
 	"math/rand"
-	"time"
+	"os"
 )
 
-// Функция для генерации порождающей матрицы G
-func generateGeneratorMatrix(k, n int) [][]int {
-	G := make([][]int, k)
-	for i := range G {
-		G[i] = make([]int, n)
-		for j := range G[i] {
-			G[i][j] = rand.Intn(2) // Заполнение случайными 0 и 1
-		}
-	}
-	return G
+const N = 8
+
+var matrixG = [][]int{
+	{1, 0, 0, 0, 1, 1, 0},
+	{0, 1, 0, 0, 1, 0, 1},
+	{0, 0, 1, 0, 0, 1, 1},
+	{0, 0, 0, 1, 1, 1, 1},
 }
 
-// Функция для проверки невырожденности матрицы
-func isInvertible(matrix [][]int) bool {
-	// Здесь можно реализовать проверку на невырожденность
-	// Для простоты, будем считать, что матрица всегда невырождена
-	return true
+var matrixS = [][]int{
+	{1, 0, 0, 0},
+	{0, 1, 0, 0},
+	{0, 0, 1, 0},
+	{0, 0, 0, 1},
 }
 
-// Функция для генерации невырожденной матрицы S
-func generateInvertibleMatrix(k int) [][]int {
-	var S [][]int
-	for {
-		S = make([][]int, k)
-		for i := range S {
-			S[i] = make([]int, k)
-			for j := range S[i] {
-				S[i][j] = rand.Intn(2) // Заполнение случайными 0 и 1
-			}
-		}
-		if isInvertible(S) {
-			break
-		}
-	}
-	return S
+var matrixP = [][]int{
+	{1, 0, 0, 0, 0, 0, 0},
+	{0, 1, 0, 0, 0, 0, 0},
+	{0, 0, 1, 0, 0, 0, 0},
+	{0, 0, 0, 1, 0, 0, 0},
+	{0, 0, 0, 0, 1, 0, 0},
+	{0, 0, 0, 0, 0, 1, 0},
+	{0, 0, 0, 0, 0, 0, 1},
 }
 
-// Функция для генерации подстановочной матрицы P
-func generatePermutationMatrix(n int) [][]int {
-	P := make([][]int, n)
-	for i := range P {
-		P[i] = make([]int, n)
-		for j := range P[i] {
-			if i == j {
-				P[i][j] = 1
-			} else {
-				P[i][j] = 0
-			}
-		}
-	}
-	// Перемешивание строк
-	for i := range P {
-		j := rand.Intn(n)
-		P[i], P[j] = P[j], P[i]
-	}
-	return P
+var matrixGHead [][]int
+var matrixH = [][]int{
+	{1, 1, 0},
+	{1, 0, 1},
+	{0, 1, 1},
+	{1, 1, 1},
+	{1, 0, 0},
+	{0, 1, 0},
+	{0, 0, 1},
 }
 
-// Функция для умножения матриц
-func multiplyMatrices(A, B [][]int) [][]int {
-	rowsA := len(A)
-	colsA := len(A[0])
-	colsB := len(B[0])
-	result := make([][]int, rowsA)
-	for i := range result {
-		result[i] = make([]int, colsB)
-		for j := 0; j < colsB; j++ {
-			for k := 0; k < colsA; k++ {
-				result[i][j] = (result[i][j] + A[i][k]*B[k][j]) % 2 // Умножение по модулю 2
-			}
-		}
-	}
-	return result
-}
+var matrixInverseP [][]int
+var matrixInverseS [][]int
+var vectorZ []int
 
-// Функция для представления сообщения в виде двоичного вектора
-func messageToBinaryVector(message string, k int) []int {
-	binaryVector := make([]int, k)
-	for i := 0; i < k; i++ {
-		if i < len(message) {
-			binaryVector[i] = int(message[i] - '0') // Преобразуем символ в бинарный вектор
-		} else {
-			binaryVector[i] = 0 // Заполняем нулями, если сообщение короче
-		}
-	}
-	return binaryVector
-}
+func prepareAllInfo() {
+	matrixGHead = multiplyMatrices(matrixS, matrixG)
+	matrixGHead = multiplyMatrices(matrixGHead, matrixP)
 
-// Функция для умножения матрицы на вектор
-func multiplyMatrixByVector(matrix [][]int, vector []int) []int {
-	rows := len(matrix)
-	cols := len(matrix[0])
-	result := make([]int, cols)
-	for i := 0; i < rows; i++ {
-		for j := 0; j < cols; j++ {
-			result[j] = (result[j] + matrix[i][j]*vector[i]) % 2 // Умножение по модулю 2
-		}
-	}
-	return result
-}
-
-// Функция для умножения вектора на матрицу
-func multiplyVectorByMatrix(vector []int, matrix [][]int) []int {
-	// Проверяем, что размер вектора соответствует количеству строк в матрице
-	if len(vector) != len(matrix) {
-		vector = vector[:len(matrix)]
-	}
-
-	// Результирующий вектор
-	result := make([]int, len(matrix[0]))
-
-	// Умножаем вектор на матрицу
-	for i := 0; i < len(matrix); i++ {
-		for j := 0; j < len(matrix[i]); j++ {
-			if vector[i] == 1 {
-				result[j] ^= matrix[i][j]
+	for i := range matrixGHead {
+		for j := range matrixGHead[i] {
+			if matrixGHead[i][j]%2 == 0 {
+				matrixGHead[i][j] = 0
 			}
 		}
 	}
 
-	return result
+	matrixInverseP = matrixInverse(matrixP)
+	matrixInverseS = matrixInverse(matrixS)
 }
 
-// Функция для генерации случайного бинарного вектора ошибок
-func generateErrorVector(n, t int) []int {
-	Z := make([]int, n)
-	indices := rand.Perm(n)[:t] // Получаем случайные индексы для t единиц
-	for _, index := range indices {
-		Z[index] = 1
-	}
-	return Z
-}
+func encryptByte(byteForEncryption byte) []byte {
+	bitArrayFull := toBitArray([]byte{byteForEncryption})
+	byteArr := make([]byte, (N-1)*2)
+	index := 0
+	for k := 0; k < N; k += N / 2 {
+		bitArrayHalf := bitArrayFull[k : k+N/2]
+		vectorZ = getErrorVector(N-1, N/2)
 
-func Transpose(matrix [][]int) [][]int {
-	if len(matrix) == 0 {
-		return nil
-	}
-
-	// Создаем новую матрицу с размерами, поменянными местами
-	transposed := make([][]int, len(matrix[0]))
-	for i := range transposed {
-		transposed[i] = make([]int, len(matrix))
-	}
-
-	// Заполняем транспонированную матрицу
-	for i := range matrix {
-		for j := range matrix[i] {
-			transposed[j][i] = matrix[i][j]
+		arr := make([]int, len(bitArrayHalf))
+		for i, bit := range bitArrayHalf {
+			arr[i] = toBool(bit)
 		}
+
+		result := multiplyMatrixOnVector(matrixGHead, arr)
+
+		for i := range result {
+			result[i] = (result[i] + vectorZ[i]) % 2
+		}
+
+		for i := range result {
+			byteArr[index+i] = byte(result[i])
+		}
+
+		index += N - 1
 	}
 
-	return transposed
+	return byteArr
 }
 
-// Находит обратную матрицу для данной двоичной невырожденной матрицы
-func findInverseMatrixBinary(matrix [][]int) ([][]int, error) {
-	n := len(matrix)
-
-	// Проверяем, что матрица является квадратной
-	for _, row := range matrix {
-		if len(row) != n {
-			return nil, fmt.Errorf("Матрица должна быть квадратной")
+func decryptBytes(inputBytes []byte) byte {
+	bytes := make([]byte, 2)
+	boolsResult := make([]bool, N)
+	index := 0
+	for k := 0; k < (N-1)*2; k += N - 1 {
+		arr := make([]int, len(inputBytes)/2)
+		for i := k; i < k+len(inputBytes)/2; i++ {
+			arr[i-k] = int(inputBytes[i])
 		}
-	}
 
-	// Создаём единичную матрицу
-	identity := make([][]int, n)
-	for i := range identity {
-		identity[i] = make([]int, n)
-		identity[i][i] = 1
-	}
+		vectorCHead := multiplyMatrixOnVector(matrixInverseP, arr)
 
-	// Расширенная матрица
-	augmented := make([][]int, n)
-	for i := range augmented {
-		augmented[i] = make([]int, 2*n)
-		copy(augmented[i], matrix[i])
-		copy(augmented[i][n:], identity[i])
-	}
+		for i := range vectorCHead {
+			vectorCHead[i] %= 2
+		}
 
-	// Приводим расширенную матрицу к ступенчатому виду
-	for i := 0; i < n; i++ {
-		// Находим первый ненулевой элемент в i-ой строке
-		pivotRow := i
-		for j := i; j < n; j++ {
-			if augmented[j][i] == 1 {
-				pivotRow = j
+		syndrom := multiplyMatrixOnVector(matrixH, vectorCHead)
+		for i := range syndrom {
+			syndrom[i] %= 2
+		}
+
+		resultIndex := -1
+		for i := range matrixH {
+			sumIndex := 0
+			for j := range matrixH[0] {
+				if syndrom[j] == matrixH[i][j] {
+					sumIndex++
+				}
+			}
+			if sumIndex == len(matrixH[0]) {
+				resultIndex = i
 				break
 			}
 		}
 
-		// Меняем i-ую строку на pivotRow
-		augmented[i], augmented[pivotRow] = augmented[pivotRow], augmented[i]
-
-		// Делаем первый ненулевой элемент в i-ой строке равным 1
-		for j := i + 1; j < 2*n; j++ {
-			augmented[i][j] ^= 1
+		if resultIndex != -1 {
+			if vectorCHead[resultIndex] == 0 {
+				vectorCHead[resultIndex] = 1
+			} else {
+				vectorCHead[resultIndex] = 0
+			}
 		}
 
-		// Обнуляем остальные элементы в i-ом столбце
-		for j := 0; j < n; j++ {
-			if j != i && augmented[j][i] == 1 {
-				for k := 0; k < 2*n; k++ {
-					augmented[j][k] ^= augmented[i][k]
-				}
+		vectorCHead = multiplyMatrixOnVector(matrixInverseS, vectorCHead)
+
+		for i := index; i < index+len(boolsResult)/2; i++ {
+			boolsResult[i] = vectorCHead[i-index] == 1
+		}
+
+		index += N / 2
+	}
+
+	bytes[0] = fromBitArray(boolsResult)
+	return bytes[0]
+}
+
+func encryptMessageFromFile(inputFileName, outputFileName string) {
+	prepareAllInfo()
+
+	inputFile, err := os.Open(inputFileName)
+	if err != nil {
+		panic(err)
+	}
+	defer inputFile.Close()
+
+	outputFile, err := os.Create(outputFileName)
+	if err != nil {
+		panic(err)
+	}
+	defer outputFile.Close()
+
+	var buf [1]byte
+	for {
+		_, err := inputFile.Read(buf[:])
+		if err != nil {
+			break
+		}
+		encryptedBytes := encryptByte(buf[0])
+		_, err = outputFile.Write(encryptedBytes)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func decryptMessageFromFile(inputFileName, outputFileName string) {
+	inputFile, err := os.Open(inputFileName)
+	if err != nil {
+		panic(err)
+	}
+	defer inputFile.Close()
+
+	outputFile, err := os.Create(outputFileName)
+	if err != nil {
+		panic(err)
+	}
+	defer outputFile.Close()
+
+	var buf = make([]byte, (N-1)*2)
+	for {
+		_, err := inputFile.Read(buf)
+		if err != nil {
+			break
+		}
+		decryptedByte := decryptBytes(buf)
+		_, err = outputFile.Write([]byte{decryptedByte})
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func main() {
+	encryptMessageFromFile("input.txt", "output.txt")
+	decryptMessageFromFile("output.txt", "decrypted.txt")
+}
+
+func getErrorVector(length, maxIndex int) []int {
+	arr := make([]int, length)
+	index := rand.Intn(maxIndex)
+	arr[index] = 1
+	return arr
+}
+
+func matrixCreate(rows, cols int) [][]int {
+	result := make([][]int, rows)
+	for i := range result {
+		result[i] = make([]int, cols)
+	}
+	return result
+}
+
+func multiplyMatrices(matrixA, matrixB [][]int) [][]int {
+	aRows, aCols := len(matrixA), len(matrixA[0])
+	bRows, bCols := len(matrixB), len(matrixB[0])
+
+	if aCols != bRows {
+		panic("Non-conformable matrices in multiplyMatrices")
+	}
+
+	result := matrixCreate(aRows, bCols)
+	for i := range result {
+		for j := range result[i] {
+			for k := 0; k < aCols; k++ {
+				result[i][j] += matrixA[i][k] * matrixB[k][j]
 			}
 		}
 	}
 
-	// Правая часть расширенной матрицы содержит обратную матрицу
-	inverse := make([][]int, n)
-	for i := 0; i < n; i++ {
-		inverse[i] = make([]int, n)
-		copy(inverse[i], augmented[i][n:])
-	}
-
-	return inverse, nil
+	return result
 }
 
-func main() {
-	rand.Seed(time.Now().UnixNano())
-
-	// Параметры системы
-	k := 4  // Пример значения
-	n := 12 // Пример значения
-	t := 2  // Пример значения
-	print(n)
-
-	// Шаг 1: Генерация порождающей матрицы G
-	//G := generateGeneratorMatrix(k, n)
-	//fmt.Println("Порождающая матрица G:")
-	//for _, row := range G {
-	//	fmt.Println(row)
-	//}
-	G := [][]int{
-		{0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0},
-		{0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0},
-		{1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0},
+func matrixDuplicate(matrix [][]int) [][]int {
+	result := matrixCreate(len(matrix), len(matrix[0]))
+	for i := range matrix {
+		for j := range matrix[i] {
+			result[i][j] = matrix[i][j]
+		}
 	}
-
-	// Шаг 2: Генерация невырожденной матрицы S
-	//S := generateInvertibleMatrix(k)
-	//fmt.Println("Невырожденная матрица S:")
-	//for _, row := range S {
-	//	fmt.Println(row)
-	//}
-	S := [][]int{
-		{1, 0, 0, 1},
-		{0, 1, 0, 1},
-		{0, 1, 0, 0},
-		{0, 0, 1, 1},
-	}
-
-	// Шаг 3: Генерация подстановочной матрицы P
-	//P := generatePermutationMatrix(n)
-	//fmt.Println("Подстановочная матрица P:")
-	//for _, row := range P {
-	//	fmt.Println(row)
-	//}
-	P := [][]int{
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
-		{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-		{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-		{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-	}
-
-	// Шаг 4: Вычисление G1 = S * G * P
-	GG_new := multiplyMatrices(S, G)
-	GG := multiplyMatrices(GG_new, P)
-	fmt.Println("Открытый ключ G1:")
-	for _, row := range GG {
-		fmt.Println(row)
-	}
-
-	// Открытый ключ (G1, t) и секретный ключ (S, G, P)
-	fmt.Println("Открытый ключ: (GG, t) =", GG, t)
-	fmt.Println("Секретный ключ: (S, G, P) =", S, G, P)
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	message := "1010" // Двоичное сообщение длиной k/
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	// Шаг 1: Представить M в виде двоичного вектора длины k
-	M := messageToBinaryVector(message, k)
-	fmt.Println("Сообщение M в двоичном векторе:", M)
-
-	// Шаг 2: Выбрать случайный бинарный вектор ошибок Z длиной n
-	Z := generateErrorVector(n, t)
-	fmt.Println("Случайный бинарный вектор ошибок Z:", Z)
-	// Шаг 4: Вычислить бинарный вектор C = M · G_A + Z
-	C := multiplyVectorByMatrix(M, GG)
-
-	for i := 0; i < len(C); i++ {
-		C[i] = (C[i] + Z[i]) % 2 // Сложение по модулю 2
-	}
-
-	fmt.Println("Зашифрованный вектор C:", C)
-
-	P_1 := Transpose(P)
-
-	S_1 := Transpose(S)
-
-	fmt.Println("обратная к S_1:")
-	for _, row := range S_1 {
-		fmt.Println(row)
-	}
-
-	C_1 := multiplyVectorByMatrix(C, P_1)
-	fmt.Println("C_1 vector:")
-	fmt.Println(C_1)
-	Y := multiplyVectorByMatrix(Z, P_1)
-	fmt.Println("error vector after P_1:")
-	fmt.Println(Y)
-	for i := 0; i < len(C_1); i++ {
-		C_1[i] = (Y[i] + C_1[i]) % 2
-	}
-	fmt.Println("C_1 vector after sum with Y:")
-	fmt.Println(C_1)
-
-	fmt.Println("G_pseudo:")
-	G_pseudo := findPseudoinverse(G)
-
-	C_1 = multiplyVectorByMatrix(C_1, G_pseudo)
-	fmt.Println("C_1 vector:")
-	fmt.Println(C_1)
-	//S_1 = [][]int{{1, -1, 1, 0}, {0, 0, 1, 0}, {0, -1, 1, 1}, {0, 1, -1, 0}}  its inv matrix for S
-	M_recieved := multiplyMatrixByVector(S_1, C_1)
-
-	//M_recieved := multiplyVectorByMatrix(C_1, S_1)
-	fmt.Println("MSG:")
-	fmt.Println(M_recieved)
-
-	E := multiplyMatrices(S, S_1)
-	fmt.Println(E)
-	E = multiplyMatrices(G, G_pseudo)
-	fmt.Println(E)
-
+	return result
 }
 
-func findPseudoinverse(G [][]int) [][]int {
-	G_T := Transpose(G)
+func matrixDecompose(matrix [][]int) ([][]int, []int, int) {
+	n := len(matrix)
+	result := matrixDuplicate(matrix)
+	perm := make([]int, n)
+	for i := range perm {
+		perm[i] = i
+	}
+	toggle := 1
 
-	// Вычисляем G^T * G
-	G_T_G := multiplyMatrices(G_T, G)
+	for j := 0; j < n-1; j++ {
+		colMax := math.Abs(float64(result[j][j]))
+		pRow := j
+		for i := j + 1; i < n; i++ {
+			if math.Abs(float64(result[i][j])) > colMax {
+				colMax = math.Abs(float64(result[i][j]))
+				pRow = i
+			}
+		}
 
-	// Вычисляем обратную матрицу (G^T * G)^-1
-	G_T_G_Inv := Transpose(G_T_G)
+		if pRow != j {
+			result[pRow], result[j] = result[j], result[pRow]
+			perm[pRow], perm[j] = perm[j], perm[pRow]
+			toggle = -toggle
+		}
 
-	// Вычисляем псевдообратную матрицу G^+
-	G_Plus := multiplyMatrices(multiplyMatrices(G_T_G_Inv, G_T), Transpose(G))
+		if math.Abs(float64(result[j][j])) < 1e-20 {
+			return nil, nil, 0
+		}
+		for i := j + 1; i < n; i++ {
+			result[i][j] /= result[j][j]
+			for k := j + 1; k < n; k++ {
+				result[i][k] -= result[i][j] * result[j][k]
+			}
+		}
+	}
 
-	return G_Plus
+	return result, perm, toggle
+}
+
+func matrixInverse(matrix [][]int) [][]int {
+	n := len(matrix)
+	result := matrixDuplicate(matrix)
+	lum, perm, _ := matrixDecompose(matrix)
+	if lum == nil {
+		panic("Unable to compute inverse")
+	}
+	b := make([]int, n)
+	for i := range perm {
+		for j := 0; j < n; j++ {
+			if i == perm[j] {
+				b[j] = 1
+			} else {
+				b[j] = 0
+			}
+		}
+		x := helperSolve(lum, b)
+		for j := range x {
+			result[j][i] = x[j]
+		}
+	}
+	return result
+}
+
+func multiplyMatrixOnVector(matrix [][]int, vector []int) []int {
+	result := make([]int, len(matrix[0]))
+	for i := range result {
+		for j := range matrix {
+			result[i] += vector[j] * matrix[j][i]
+		}
+	}
+	return result
+}
+
+func toBitArray(bytes []byte) []bool {
+	result := make([]bool, len(bytes)*8)
+	for i, b := range bytes {
+		for j := 0; j < 8; j++ {
+			result[i*8+j] = b&(1<<uint(j)) != 0
+		}
+	}
+	return result
+}
+
+func fromBitArray(bits []bool) byte {
+	var result byte
+	for i, b := range bits {
+		if b {
+			result |= 1 << uint(i)
+		}
+	}
+	return result
+}
+
+func toBool(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func helperSolve(luMatrix [][]int, b []int) []int {
+	n := len(luMatrix)
+	x := make([]int, n)
+	copy(x, b)
+	for i := 1; i < n; i++ {
+		sum := x[i]
+		for j := 0; j < i; j++ {
+			sum -= luMatrix[i][j] * x[j]
+		}
+		x[i] = sum
+	}
+
+	x[n-1] /= luMatrix[n-1][n-1]
+	for i := n - 2; i >= 0; i-- {
+		sum := x[i]
+		for j := i + 1; j < n; j++ {
+			sum -= luMatrix[i][j] * x[j]
+		}
+		x[i] = sum / luMatrix[i][i]
+	}
+
+	return x
 }
